ea3c27db46f55e60c0ccb845f50994b46693d51e
HUY212212121212121 <90818281+HUY212212121212121@users.noreply.github.com>
2022-07-27 13:07:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
 <html>
 <body>
 
-<?php
-echo "My first PHP script!";
-?>
+<h1>Welcome</h1>
 
+<?php
+echo "Huy toys store";
+?>
+<?php
+$t = date("27");
+
+if ($t < "26") {
+    echo "Have a good day!";
+} else {
+    echo "Have a good night!";
+}
+?>
 </body>
 </html>
