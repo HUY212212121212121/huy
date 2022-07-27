ce35ed883162bc88ddcae03210dd8da14a60bfb2
HUY212212121212121 <90818281+HUY212212121212121@users.noreply.github.com>
2022-07-27 13:25:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
 <html>
 <body>
+body {
+background-image: url('https://scontent.fsgn5-3.fna.fbcdn.net/v/t1.6435-1/179240500_1099117690582511_1614432114218035177_n.jpg?stp=dst-jpg_p240x240&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=T9eOm-FgPFcAX9HJ7pJ&_nc_ht=scontent.fsgn5-3.fna&oh=00_AT8-nFeD_DqaEZ11WXG3hkAhUSWkC1kJBd0N45lVtfr-Pw&oe=630658CD');
+}
+</style>
+</head>
+<body>
 
-<h1>Welcome</h1>
+<h1>Welcome to huy store</h1>
 
 <?php
 echo "Huy toys store</br>";
@@ -10,12 +16,12 @@ echo "Huy toys store</br>";
 
 
 <?php
-$t = date("27");
+$t = date("19");
 
-if ($t < "26") {
-    echo "Have a good day!";
+if ($t < "17") {
+    echo "We open!<>";
 } else {
-    echo "Have a good night!";
+    echo "We close!";
 }
 ?>
 </body>
