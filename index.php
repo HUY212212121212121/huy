09f50e537358edbd7e363b84f0519109a6b0e909
HUY212212121212121 <90818281+HUY212212121212121@users.noreply.github.com>
2022-08-16 14:16:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,32 @@
 <body>
 <style> background-image: url ('img_girl.jpg'); </style>
 
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #dddddd;
+}
 
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  padding: 8px;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
 <h1>Welcome to huy store</h1>
 
 <?php
